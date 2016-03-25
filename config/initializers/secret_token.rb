31b6034a8970e0002d8006b51ce14618b11d814c
Jekie <evgenij.kulikov@gmai.com>
2016-03-25 18:14:00 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anyapp::Application.config.secret_key_base = '11a90150a895dd38d5a1a3cc97ad1fb6baad25bbec1e6c9b0f501b24145f0cfb10dcff0339835918f8128064e7658ea8d045d224ffcfe577f3a9643f4b17d23c'
